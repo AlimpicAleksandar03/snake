151ad96879a6f7c1b0cd64a85f6f7a99c6ee1006
Aleksandar Alimpic <alimpicaleksandar@192.168.1.9>
2022-09-08 03:24:17 +0200

    moving one square

--- main.js
+++ main.js
@@ -1,87 +1,83 @@
-let snakeboard = document.getElementById('gameCanvas')
+let snake_speed = 1
+let lastRenderTime = 0
+let snakeSpeed = 3
+let snakeBody = [{ x: 10, y: 0 }]
+let size = 10
+let gameBoard = document.getElementById('gameCanvas')
 let ctx = gameCanvas.getContext('2d')
-let squareSize = 10
-let snakePoints = [{ y: 10, x: 10 }]
-
-let direction = 'right'
-// if we are moving right then only directions available are up and down
-// if we are moving left then only directions available are up and down
-// if we are moving down then only directions available are left and right
-// if we are moving up then only directions available are left and right
+let currentDirection = { x: 10, y: 0 } //right
+let previousDirection = { x: 0, y: 0 }
+let boundaries = gameBoard.getBoundingClientRect()
+let food = { x: 50, y: 50 }
+let around = [
+  { x: 0, y: -10 }, //down
+  { x: 0, y: 10 }, // up
+  { x: 10, y: 0 }, //right
+  { x: -10, y: 0 }, //left
+]
 
 ctx.fillRect(0, 0, gameCanvas.clientWidth, gameCanvas.clientHeight)
-let snakeboardPosition = snakeboard.getBoundingClientRect()
 
-function drawApple() {}
-function drawSnake(points) {
-  points.forEach((point) => {
-    ctx.fillStyle = 'lightgreen'
-    ctx.fillRect(point.x, point.y, squareSize, squareSize)
+function runGame(currentTime) {
+  window.requestAnimationFrame(runGame)
+  let secondsAfterRender = (currentTime - lastRenderTime) / 1000
+  if (secondsAfterRender < 1 / snakeSpeed) return
+  lastRenderTime = currentTime
+  updateSnake(snakeBody)
+  drawSnake(snakeBody)
+}
+drawFood(food)
+
+window.requestAnimationFrame(runGame)
+
+function drawSnake(snakeBody) {
+  snakeBody.forEach(({ x, y }) => {
+    ctx.fillStyle = 'blue'
+    ctx.fillRect(x, y, size, size)
   })
 }
-function moveSnake(points) {
-  setInterval(() => {
-    points.forEach((point) => {
-      if (checkWallCollision(point)) {
-        document.location.reload()
-      }
-      let oldPoint = Object.assign({}, point)
-      killOldPoint(oldPoint) // this is the actual moving, we are just manipulating with colors
-      // now based on direction we are moving we will increase or decrease coordinates
-      if (direction === 'right') {
-        point.x += 10
-        return
-      } else if (direction === 'left') {
-        point.x -= 10
-        return
-      } else if (direction === 'up') {
-        point.y -= 10
-        return
-      } else if (direction === 'down') {
-        point.y += 10
-        return
-      }
-      // that way the length of a lightgreen colored points (our snake) is constant (every time we kill one, we make new one 10 points greater)
-    })
-    drawSnake(points)
-  }, 500)
+function drawFood(food) {
+  ctx.fillStyle = 'red'
+  ctx.fillRect(food.x, food.y, size, size)
+}
+function updateSnake(snakeBody) {
+  snakeBody.forEach((point, i) => {
+    let prevX = point.x
+    let prevY = point.y
+    point.x = point.x + getInputDirection().x
+    point.y = point.y + getInputDirection().y
+    ctx.fillStyle = 'black'
+    ctx.fillRect(prevX, prevY, size, size)
+  })
 }
 
-function killOldPoint(oldPoint) {
-  ctx.fillStyle = 'black'
-  ctx.fill()
-  ctx.fillRect(oldPoint.x, oldPoint.y, squareSize, squareSize)
-}
-document.addEventListener('keydown', function (event) {
-  if (event.keyCode == 37) {
-    direction = 'left'
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowUp':
+      if (previousDirection.y !== 0) break
 
-    // smanjti x koordinatu
-  } else if (event.keyCode == 39) {
-    direction = 'right'
+      currentDirection = around[0]
+      break
 
-    // povecati x koordinatu
-  } else if (event.keyCode == 38) {
-    direction = 'up'
+    case 'ArrowDown':
+      if (previousDirection.y !== 0) break
 
-    // smanjiti y koordinatu
-  } else if (event.keyCode == 40) {
-    direction = 'down'
+      currentDirection = around[1]
+      break
 
-    // povecati y koordinaty
+    case 'ArrowRight':
+      if (previousDirection.x !== 0) break
+
+      currentDirection = around[2]
+      break
+    case 'ArrowLeft':
+      if (previousDirection.x !== 0) break
+      currentDirection = around[3]
+      break
   }
 })
 
-moveSnake(snakePoints)
-
-function checkWallCollision(point) {
-  console.log(point)
-  return (
-    point.x === snakeboardPosition.right - 20 ||
-    point.y === snakeboardPosition.bottom - 20 ||
-    point.x === 0 ||
-    point.y === 0
-  )
+function getInputDirection() {
+  previousDirection = currentDirection
+  return currentDirection
 }
-
-// drawSnake(snakePoints)
